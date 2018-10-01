d834f74f916fe724eeec79f6c5c53cf29bb3cea7
Acaeris <acaeris@gmail.com>
2018-10-01 17:12:47 +0100

    Improved Input class bindings

--- src/app/modules/elements/input/input.component.ts
+++ src/app/modules/elements/input/input.component.ts
@@ -8,6 +8,9 @@ import {
   HostBinding
 } from '@angular/core';
 
+import { isPresent } from '../../../libs/isPresent';
+import { hasValue } from '../../../libs/hasValue';
+
 /**
  * Implementation of Input component
  *
@@ -20,44 +23,35 @@ import {
   host: { 'class' : 'ui input' }
 })
 export class SemanticInputComponent implements OnChanges {
-  @Input('focus') focus?: boolean;
-  @Input('loading') loading?: boolean;
-  @Input('type') type?: string;
-  @Input('icon') icon?: string;
-  @Input('icon-side') iconSide?: string;
-  @Input('flag') flag?: string;
-  @Input('placeholder') placeholder?: string;
-  @Input('value') value?: string;
+  @Input() focus?: boolean;
+  @Input() loading?: boolean;
+  @Input() type?: string;
+  @Input() icon?: string;
+  @Input() iconSide?: string;
+  @Input() flag?: string;
+  @Input() placeholder?: string;
+  @Input() value?: string;
   @HostBinding('class.focus')
   get isFocused() {
-    return this.isPresent(this.focus);
+    return isPresent(this.focus);
+  }
+  @HostBinding('class.icon')
+  get hasIcon() {
+    return hasValue(this.icon) || hasValue(this.flag);
   }
   @HostBinding('class.loading')
   get isLoading() {
-    return this.isPresent(this.loading);
+    return isPresent(this.loading);
   }
 
   constructor(private el:ElementRef, private renderer: Renderer2) { }
 
   ngOnChanges() {
-    if (!this.hasValue(this.value)) {
+    if (!hasValue(this.value)) {
       this.value = "";
     }
-    if (this.hasValue(this.icon) || this.hasValue(this.flag)) {
-      if (this.hasValue(this.iconSide)) {
-        this.renderer.addClass(this.el.nativeElement, this.iconSide);
-      }
-      this.renderer.addClass(this.el.nativeElement, 'icon');
-    } else {
-      this.renderer.removeClass(this.el.nativeElement, 'icon');
+    if ((hasValue(this.icon) || hasValue(this.flag)) && hasValue(this.iconSide)) {
+      this.renderer.addClass(this.el.nativeElement, this.iconSide);
     }
-  }
-
-  isPresent(key?: boolean) {
-    return "undefined" !== typeof key && !key;
-  }
-
-  hasValue(key?: string) {
-    return "undefined" !== typeof key && key !== "";
   }
 }
